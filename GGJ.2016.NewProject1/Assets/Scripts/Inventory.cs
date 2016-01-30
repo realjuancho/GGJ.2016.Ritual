a2605298defd6f2b1e6ed4be5e4f11216bc50104
Boshi <ipus2003@gmail.com>
2016-01-30 09:46:08 -0600

    Sistema de inventario importado

---
+++ GGJ.2016.NewProject1/Assets/Scripts/Inventory.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item {
+	public int id;
+
+	public Item(int itemID){
+		id = itemID;
+	}
+
+}
+
+public class Inventory : MonoBehaviour {
+
+	public string[] itemNames;
+	public Sprite[] itemIcons;
+
+	public static string[] names;
+	public static Sprite[] icons;
+	public static List<Item> inventory;
+
+	void Start () {
+		names = itemNames;
+		icons = itemIcons;
+		inventory = new List<Item>();
+	}
+
+	public static void addItem (Item item) {
+		inventory.Add(item);
+	}
+
+	public static bool removeItem (Item item) {
+		foreach (Item slot in inventory){
+			if (slot.id == item.id) {
+				inventory.RemoveAt(slot);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static bool checkForItem (Item item){
+		foreach (Item slot in inventory){
+			if (slot.id == item.id) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static int getSlotID (int slot){
+		return inventory[slot].id;
+	}
+	public static string getName(int itemID){
+		return names[itemID];
+	}
+	public static Sprite getIcon(int itemID){
+		return icons[itemID];
+	}
+
+
+	/*----------------------DEBUG------------------*/
+
+	public static void displayInventory(){
+		Debug.Log("----Inventory----\n");
+		foreach (Item slot in inventory){
+			Debug.Log("Item : " + slot.id + " Amount: " + slot.amount + "\n");
+		}
+		Debug.Log("-----------------\n");
+	}
+
+	/*----------------------END------------------*/
+}
---
+++ GGJ.2016.NewProject1/Assets/Scripts/InventoryUIManagement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InventoryUIManagement : MonoBehaviour {
+
+	public GameObject UIItem;
+
+	int numberOfItems;
+
+	// Use this for initialization
+	void Start () {
+		numberOfItems = 0;
+		refreshUIInventory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		refreshUIInventory();
+	}
+
+	void refreshUIInventory (){
+		removeAllUIItems();
+		foreach (Item slot in Inventory.inventory){
+			if(slot.id != 0){
+				addUIItem(slot);
+			}
+		}
+	}
+
+	void addUIItem (Item item){
+		GameObject clone = Instantiate(UIItem) as GameObject;
+		clone.transform.SetParent(gameObject.transform, false);
+		clone.GetComponent<RectTransform>().anchoredPosition = new Vector2(clone.GetComponent<RectTransform>().anchoredPosition.x, (-60 * numberOfItems) -10);
+		clone.GetComponent<UnityEngine.UI.Image>().sprite = Inventory.getIcon(item.id);
+		clone.transform.Find("Item Name").GetComponent<UnityEngine.UI.Text>().text = Inventory.getName(item.id);
+		numberOfItems++;
+	}
+	void removeAllUIItems (){
+		GameObject[] objects = GameObject.FindGameObjectsWithTag("UIInventoryItem");
+		for (int i=0; i<objects.Length; i++){
+    		Destroy(objects[i]);
+    	}
+    	numberOfItems = 0;
+	}
+}
