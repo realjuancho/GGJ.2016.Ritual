0255cdf9d9a31b1742269cbea93551ab74de29f6
Boshi <ipus2003@gmail.com>
2016-01-30 07:28:46 -0600

    Menu listo

---
+++ GGJ.2016.NewProject1/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+	public Slider volumeSlider;
+	public Button exitButton;
+	public Button returnButton;
+
+	void Start (){
+		volumeSlider.onValueChanged.AddListener(ChangeValue);
+		ChangeValue(volumeSlider.value);
+		exitButton.onClick.AddListener (exitGame);
+		returnButton.onClick.AddListener (closeMenu);
+	}
+	void ChangeValue(float newValue) {
+		AudioListener.volume = newValue;
+	}
+	void exitGame() {
+		Application.Quit();
+	}
+	void closeMenu(){
+		Destroy (GameObject.FindGameObjectWithTag ("Menu"));
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# GGJ.2016.Ritual
-[![Gitter](https://badges.gitter.im/realjuancho/GGJ.2016.Ritual.svg)](https://gitter.im/realjuancho/GGJ.2016.Ritual?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
