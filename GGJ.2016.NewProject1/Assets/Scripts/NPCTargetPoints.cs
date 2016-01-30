35ee06499054e39197ffa79f64a86f871f34be53
Juan Cerda <realjuancho@gmail.com>
2016-01-30 05:35:59 -0600

    no message

---
+++ GGJ.2016.NewProject1/Assets/Scripts/NPCTargetPoints.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCTargetPoints : MonoBehaviour {
+
+	public Hash.LocationNames location = Hash.LocationNames.Dormitorio;
+	public NPCTarget currentTarget;
+	public NPCTarget[] NPCTargetPointsFound;
+
+	int currentPoint = 0;
+
+	void Start()
+	{
+	 	NPCTargetPointsFound = GetComponentsInChildren<NPCTarget>();
+
+	 	if(NPCTargetPointsFound.Length > 0)
+	 	{
+	 		currentTarget = NPCTargetPointsFound[0];
+	 		currentPoint = 0;
+	 	}
+	}
+
+	// Update is called once per frame
+
+	public NPCTarget GetCurrentNPCTarget()
+	{
+		return currentTarget;
+	}
+
+
+
+	public void moveCurrentPoint()
+	{
+		if(currentPoint < NPCTargetPointsFound.Length -1)
+		{
+			currentPoint++;
+		}
+		else
+		{
+			currentPoint = 0;
+		}
+
+		currentTarget = NPCTargetPointsFound[currentPoint];
+	}
+
+}
---
+++ GGJ.2016.NewProject1/Assets/Scripts/NPCVisionCone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCVisionCone : MonoBehaviour {
+
+
+	public bool grandmaFound = false;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		//TODO
+		if(col.gameObject.tag == Hash.Tags.Player)
+		{
+			grandmaFound = true;
+
+		}
+
+		Debug.Log(col.name);
+	}
+}
