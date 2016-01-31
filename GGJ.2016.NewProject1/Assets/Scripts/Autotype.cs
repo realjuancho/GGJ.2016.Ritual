1bdbfdcac0b31c09bf57d55029a48aa5a15547b2
Boshi <ipus2003@gmail.com>
2016-01-31 02:37:42 -0600

    Animaciones de NPC listas!

--- GGJ.2016.NewProject1/Assets/Scripts/Autotype.cs
+++ GGJ.2016.NewProject1/Assets/Scripts/Autotype.cs
@@ -7,19 +7,16 @@ public class AutoType : MonoBehaviour {
 	public float letterPause = 0.2f;
 	public AudioClip sound;
 
-<<<<<<< HEAD
-=======
 	public static void writeText(string text ){
 		//instance.StartCoroutine(TypeText("Hola soy un Text"));
 	}
->>>>>>> 813c2c1602342c05a8a635c47f6d00006b3a6b49
 	void Start () {
 		writeText("Hola soy un Texto");
 	}
 
-	void writeText(string text ){
-		StartCoroutine(TypeText(text));
-	}
+	//void writeText(string text ){
+	//	StartCoroutine(TypeText(text));
+	//}
 	IEnumerator TypeText (string message) {
 		GetComponent<Text>().text = "";
 		foreach (char letter in message.ToCharArray()) {
